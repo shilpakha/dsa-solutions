b8b5e4c6b0eb61c2d30b5a028a1157364de7ddc2
Shilpa Khadilkar <shilpa.khadilkar1@gmail.com>
2025-09-07 16:01:19 +0530

    Add Two Sum Solution

---
+++ Easy/twoSum.js
@@ -0,0 +1,17 @@
+// LeetCode: Two Sum (Easy)
+// https://leetcode.com/problems/two-sum/
+
+var twoSum = function(nums, target) {
+    const map = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        let complement = target - nums[i];
+        if (map.has(complement)) {
+            return [map.get(complement), i];
+        }
+        map.set(nums[i], i);
+    }
+    return [];
+};
+
+// Example run
+console.log(twoSum([2,7,11,15], 9)); // [0,1]
